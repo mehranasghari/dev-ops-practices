86eb045d4169587d3de7058ddccd05cc504ab6fb
mehran <mhr.as1317@gmail.com>
2022-07-17 13:28:43 +0430

    remove disk-analyzer.sh

--- disk-analyzer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-free=$(df -h / | grep / | awk '{print $4}')
-total=$(df -h / | grep / | awk '{print $2}')
-percent=$(df -h / | grep / | awk '{print $5}' | sed 's/%//g')
-
-echo "Free/Total memory: $free / $total"
-echo "Percentage: $percent"
